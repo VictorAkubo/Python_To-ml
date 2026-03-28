7cda18077ecb1784ec6c3540af0804ab7a40f3c4
victor akubo <victorugbede89@gmail.com>
2026-03-28 17:17:10 +0100

    keywords and so on

---
+++ keywords.py
@@ -0,0 +1,49 @@
+"""
+boolean
+
+False
+True
+None
+
+logical
+and
+or
+not
+
+conditional
+if
+elif 
+else
+
+loops
+for
+while
+break
+continue
+
+value in
+in
+not in
+
+#value is 
+is
+is not
+
+#function
+def
+return
+
+#scope
+class
+object
+
+#risk code
+try
+except
+finally
+
+#imoortation
+import 
+from
+as
+"""
---
+++ typeconversion.py
@@ -0,0 +1,3 @@
+#(1)implicit: this is when python says hey am gonna convert the sum of two diffrent datatypes to a type like example converting a sum of int + float = float so its just the work of a basic calculator
+
+#(2)explicit: is when tge programmer converts datatypes to different types using built in methods example(int(),str(),float(),list(),tuple(),set(),bool())
